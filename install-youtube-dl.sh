34105f1e26955b01d66eddecb3043d7154795d1e
Walter Dundore(admin) <walter@uwg-ojdla.uwg.westga.edu>
2022-01-19 13:39:16 -0500

    Cleaned up and renamed files

--- install-youtube-dl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-which -s brew
-if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    brew update
-fi
--- youtube-dl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]
-    then echo "Requires the URL of the youtube video as an argument"
-  exit
-fi
-
-youtube-dl --extract-audio --audio-format mp3 --prefer-ffmpeg -o '$HOME/Downloads/%(title)s-%(id)s.%(ext)s' $1
