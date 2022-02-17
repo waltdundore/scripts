bdaabff20a2a3fe4772d050bddcc1a1489a60e1a
root <root@its-foreman01.westga.edu>
2022-02-17 16:07:00 -0500

    added 2.5

---
+++ foreman/katello2.5_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+subscription-manager repos --disable "*"
+
+subscription-manager repos --enable=rhel-7-server-rpms \
+--enable rhel-7-server-optional-rpms \
+--enable rhel-server-rhscl-7-rpms
+
+yum localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+yum clean all
+
+yum localinstall https://yum.theforeman.org/releases/2.5/el7/x86_64/foreman-release.rpm
+yum localinstall https://yum.theforeman.org/katello/4.1/katello/el7/x86_64/katello-repos-latest.rpm
+yum localinstall https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
+yum install -y centos-release-scl-rh
+
+yum clean all
+yum -y update
+
