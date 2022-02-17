c53c3243b62640e66f00dc364d0a933c0851e417
Walter Dundore <walt@dundore.org>
2022-02-16 20:24:16 -0500

    update katello install

--- foreman/katello_installer.sh
+++ foreman/katello_installer.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-foreman-rake katello:upgrade_check
+# Katello-repos will upgrade the Pulp repo
+yum update foreman-release katello-repos
+# Ensure you have correct metadata
+yum clean expire-cache
+# Update all packages
+yum update -y
+# Run the installer to make all service changes
+foreman-installer
+
 systemctl daemon-reload
-foreman-installer --scenario katello
 hash -d foreman-maintain service 2> /dev/null
 foreman-maintain service restart
