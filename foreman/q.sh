ab363b72fb8a243d0981303c0a9558df4b50745f
Walter Dundore <walt@dundore.org>
2022-03-01 22:13:38 -0500

    updates

--- foreman/q.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-qpid-stat -q --ssl-certificate=/etc/pki/pulp/qpid/client.crt -b amqps://localhost:5671 resource_manager
-
-echo "if acquires is smaller than total-enqueues and acquires is not increasing over the time (while total-enqueues is), you most probably hit the deadlock /pulp tasks stuck."
